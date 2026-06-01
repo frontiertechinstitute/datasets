78b54ed1e9e588760e8cee156b6121ac227711ed
John Dong <john.dong@duke.edu>
2026-06-01 15:25:39 -0400

    virtual extremism dataset (national security)

---
+++ Virtual Extremism in US 2015-2017/data/make.R
@@ -0,0 +1,7 @@
+load("Virtual Extremism in US 2015-2017/raw/ICPSR_37679-Survey Wave 1/DS0001/37679-0001-Data.rda")
+load("Virtual Extremism in US 2015-2017/raw/ICPSR_37679-Survey Wave 2/DS0002/37679-0002-Data.rda")
+load("Virtual Extremism in US 2015-2017/raw/ICPSR_37679-Survey Wave 3/DS0003/37679-0003-Data.rda")
+
+write.csv(da37679.0001, "Virtual Extremism in US 2015-2017/data/survey_data_1")
+write.csv(da37679.0002, "Virtual Extremism in US 2015-2017/data/survey_data_2")
+write.csv(da37679.0003, "Virtual Extremism in US 2015-2017/data/survey_data_3")
